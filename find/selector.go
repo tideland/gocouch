90a02a16e7072af1f3afc7eb0c3119c6dcaacbb0
Frank Mueller <mue@tideland.biz>
2017-08-16 17:07:25 +0200

    More selector implementation

--- find/selector.go
+++ find/selector.go
@@ -17,16 +17,32 @@ import (
 )
 
 //--------------------
+// CONSTANTS
+//--------------------
+
+// CombinationOperator sets how to combine multiple selectors.
+type CombinationOperator int
+
+const (
+	CombineAnd CombinationOperator = iota + 1
+	CombineOr
+	CombineNone
+)
+
+//--------------------
 // FIND SELECTOR
 //--------------------
 
 // Selector contains one or more conditions to find documents.
 type Selector interface {
-	// SubSelectors combines extra created selectors to a new one.
-	SubSelectors(sels ...Selector) Selector
-
 	// Equal checks if the field is equal to the argument.
 	Equal(field string, argument interface{}) Selector
+
+	// In checks if the field is in the arguments.
+	In(field string, arguments ...interface{}) Selector
+
+	// GreaterThan checks if the field is greater than the argument.
+	GreaterThan(field, argument interface{})
 
 	// Marshaler allows to write a selector in its JSON encoding.
 	json.Marshaler
@@ -39,34 +55,54 @@ type selector struct {
 	arguments []interface{}
 }
 
-// NewAndSelector returns a new selector combining the called
-// operators with "and".
-func NewAndSelector() Selector {
-	return &selector{
-		operator: "$and",
+// NewSelector creates a selector based on the given
+// combination operator.
+func NewSelector(co CombinationOperator, selectors ...Selector) Selector {
+	// Get combination operator.
+	ops := map[CombinationOperator]string{
+		CombineAnd:  "$and",
+		CombineOr:   "$or",
+		CombineNone: "$nor",
 	}
-}
-
-// NewOrSelector returns a new selector combining the called
-// operators with "or".
-func NewOrSelector() Selector {
-	return &selector{
-		operator: "$or",
+	op, ok := ops[co]
+	if !ok {
+		op = "$and"
 	}
-}
-
-// SubSelectors implements Selector.
-func (s *selector) SubSelectors(selectors ...Selector) Selector {
-	for _, sub := range selectors {
-		s.arguments = append(s.arguments, sub)
+	// Create selector.
+	s := &selector{
+		operator: op,
+	}
+	for _, subselector := range selectors {
+		s.arguments = append(s.arguments, subselector)
 	}
 	return s
 }
 
+// Equal implements Selector.
 func (s *selector) Equal(field string, argument interface{}) Selector {
 	s.arguments = append(s.arguments, &selector{
 		field:     field,
 		operator:  "$eq",
+		arguments: []interface{}{argument},
+	})
+	return s
+}
+
+// In implements Selector.
+func (s *selector) In(field string, arguments ...interface{}) Selector {
+	s.arguments = append(s.arguments, &selector{
+		field:     field,
+		operator:  "$in",
+		arguments: arguments,
+	})
+	return s
+}
+
+// GreaterThan implements Selector.
+func (s *selector) GreaterThan(field string, argument interface{}) Selector {
+	s.arguments = append(s.arguments, &selector{
+		field:     field,
+		operator:  "$gt",
 		arguments: []interface{}{argument},
 	})
 	return s
