98443f548a8dfb8f280043e07c267ef55a4a8eec
Frank Mueller <frank.mueller@akka.eu>
2017-08-22 17:03:58 +0200

    Implemented Find(), ResultSet, and first test

--- find/find.go
+++ find/find.go
@@ -12,6 +12,8 @@ package find
 //--------------------
 
 import (
+	"encoding/json"
+
 	"github.com/tideland/gocouch/couchdb"
 )
 
@@ -19,9 +21,21 @@ import (
 // API
 //--------------------
 
+// Request contains the parameters of a find request.
+// TODO(mue) Think about selector as fixed argument and
+// optional parameters.
+type Request struct {
+	Selector Selector `json:"selector"`
+	Limit    int      `json:"limit,omitempty"`
+	Skip     int      `json:"skip,omitempty"`
+	Sort     string   `json:"sort,omitempty"`
+	Fields   []string `json:"fields,omitempty"`
+}
+
 // Find returns access to the found results.
-func Find(cdb couchdb.CouchDB, sel Selector) ResultSet {
-	return nil
+func Find(cdb couchdb.CouchDB, request Request) ResultSet {
+	rs := cdb.Post(cdb.DatabasePath("_find"), request)
+	return newResultSet(rs)
 }
 
 //--------------------
@@ -29,7 +43,7 @@ func Find(cdb couchdb.CouchDB, sel Selector) ResultSet {
 //--------------------
 
 // Processor is a function processing the content of a found document.
-type Processor func(id, sequence string, deleted bool, revisions []string, document couchdb.Unmarshable) error
+type Processor func(document couchdb.Unmarshable) error
 
 // ResultSet contains the result set of a find call.
 type ResultSet interface {
@@ -48,6 +62,78 @@ type ResultSet interface {
 	// Do iterates over the results of a ResultSet and
 	// processes the content.
 	Do(process Processor) error
+}
+
+// resultSet implements the ResultSet interface.
+type resultSet struct {
+	rs       couchdb.ResultSet
+	response *response
+}
+
+// newResultSet returns a ResultSet.
+func newResultSet(rs couchdb.ResultSet) ResultSet {
+	frs := &resultSet{
+		rs: rs,
+	}
+	return frs
+}
+
+// IsOK implements the ResultSet interface.
+func (frs *resultSet) IsOK() bool {
+	return frs.rs.IsOK()
+}
+
+// StatusCode implements the ResultSet interface.
+func (frs *resultSet) StatusCode() int {
+	return frs.rs.StatusCode()
+}
+
+// Error implements the ResultSet interface.
+func (frs *resultSet) Error() error {
+	return frs.rs.Error()
+}
+
+// Len implements ResultSet.
+func (frs *resultSet) Len() int {
+	if err := frs.readResponse(); err != nil {
+		return -1
+	}
+	return len(frs.response.Documents)
+}
+
+// Do implements ResultSet.
+func (frs *resultSet) Do(process Processor) error {
+	if err := frs.readResponse(); err != nil {
+		return err
+	}
+	for _, doc := range frs.response.Documents {
+		unmarshableDoc := couchdb.NewUnmarshableJSON(doc)
+		if err := process(unmarshableDoc); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// readResponse lazily reads and analyzes response.
+func (frs *resultSet) readResponse() error {
+	if !frs.IsOK() {
+		return frs.Error()
+	}
+	if frs.response == nil {
+		resp := response{}
+		err := frs.rs.Document(&resp)
+		if err != nil {
+			return err
+		}
+		frs.response = &resp
+	}
+	return nil
+}
+
+type response struct {
+	Warning   string            `json:"warning"`
+	Documents []json.RawMessage `json:"docs"`
 }
 
 // EOF
--- find/find_test.go
+++ find/find_test.go
@@ -7,6 +7,19 @@
 
 package find_test
 
+import (
+	"strings"
+	"testing"
+
+	"github.com/tideland/golib/audit"
+	"github.com/tideland/golib/etc"
+	"github.com/tideland/golib/identifier"
+	"github.com/tideland/golib/logger"
+
+	"github.com/tideland/gocouch/couchdb"
+	"github.com/tideland/gocouch/find"
+)
+
 //--------------------
 // IMPORTS
 //--------------------
@@ -16,11 +29,114 @@ package find_test
 //--------------------
 
 const (
-	TemplateDBcfg = "{etc {hostname localhost}{port 5984}{database tgocouch-testing-<<DATABASE>>}{debug-logging true}}"
+	Cfg = "{etc {hostname localhost}{port 5984}{database tgocouch-testing-<<DATABASE>>}{debug-logging true}}"
 )
 
 //--------------------
 // TESTS
 //--------------------
+
+// TestSimpleFind tests calling find with a simple selector.
+func TestSimpleFind(t *testing.T) {
+	assert := audit.NewTestingAssertion(t, true)
+	cdb, cleanup := prepareFilledDatabase("find-documents", assert)
+	defer cleanup()
+
+	// Try to find some documents a simple way.
+	selector := find.SelectOr(func(os find.Selector) {
+		os.Append(find.SelectAnd(func(as find.Selector) {
+			as.LowerThan("age", 30)
+			as.Equal("active", false)
+		}))
+		os.Append(find.SelectAnd(func(as find.Selector) {
+			as.GreaterThan("age", 60)
+			as.Equal("active", true)
+		}))
+	})
+	frs := find.Find(cdb, find.Request{
+		Selector: selector,
+		Fields:   []string{"_id", "name", "age", "active"},
+	})
+	assert.NotNil(frs)
+	assert.True(frs.IsOK())
+
+	err := frs.Do(func(document couchdb.Unmarshable) error {
+		fields := struct {
+			ID     string `json:"_id"`
+			Name   string `json:"name"`
+			Age    int    `json:"age"`
+			Active bool   `json:"active"`
+		}{}
+		if err := document.Unmarshal(&fields); err != nil {
+			return err
+		}
+		assert.True((fields.Age < 30 && !fields.Active) || (fields.Age > 60 && fields.Active))
+		assert.Logf("person with name %s has age %d and activity status is %v", fields.Name, fields.Age, fields.Active)
+		return nil
+	})
+	assert.Nil(err)
+}
+
+//--------------------
+// HELPERS
+//--------------------
+
+// Description is used for the tests.
+type Description struct {
+	Title string `json:"title"`
+	Text  string `json:"text"`
+}
+
+// Person is used for the tests.
+type Person struct {
+	DocumentID       string `json:"_id,omitempty"`
+	DocumentRevision string `json:"_rev,omitempty"`
+
+	Name         string        `json:"name"`
+	Age          int           `json:"age"`
+	Active       bool          `json:"active"`
+	Descriptions []Description `json:"descriptions"`
+}
+
+// prepareFilledDatabase opens the database, deletes a possible test
+// database, creates it newly and adds some data.
+func prepareFilledDatabase(database string, assert audit.Assertion) (couchdb.CouchDB, func()) {
+	logger.SetLevel(logger.LevelDebug)
+	cfgstr := strings.Replace(Cfg, "<<DATABASE>>", database, 1)
+	cfg, err := etc.ReadString(cfgstr)
+	assert.Nil(err)
+	cdb, err := couchdb.Open(cfg)
+	assert.Nil(err)
+	rs := cdb.DeleteDatabase()
+	rs = cdb.CreateDatabase()
+	assert.True(rs.IsOK())
+
+	gen := audit.NewGenerator(audit.FixedRand())
+	docs := []interface{}{}
+	for i := 0; i < 1000; i++ {
+		first, middle, last := gen.Name()
+		doc := Person{
+			DocumentID: identifier.Identifier(last, first, i),
+			Name:       first + " " + middle + " " + last,
+			Age:        gen.Int(18, 65),
+			Active:     gen.FlipCoin(75),
+		}
+		for j := 0; j < gen.Int(3, 9); j++ {
+			description := Description{
+				Title: gen.Sentence(),
+				Text:  gen.Paragraph(),
+			}
+			doc.Descriptions = append(doc.Descriptions, description)
+		}
+		docs = append(docs, doc)
+	}
+	results, err := cdb.BulkWriteDocuments(docs)
+	assert.Nil(err)
+	for _, result := range results {
+		assert.True(result.OK)
+	}
+
+	return cdb, func() { cdb.DeleteDatabase() }
+}
 
 // EOF
