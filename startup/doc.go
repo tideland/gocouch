5f3707cd697d99d79379f7de5db4f184c1b45f87
Frank Mueller <mue@tideland.biz>
2017-09-03 18:59:43 +0200

    Add and fix some documentation

--- startup/doc.go
+++ startup/doc.go
@@ -1,13 +1,12 @@
 // Tideland Go CouchDB Client - Startup
 //
-// Copyright (C) 2016 Frank Mueller / Tideland / Oldenburg / Germany
+// Copyright (C) 2016-2017 Frank Mueller / Tideland / Oldenburg / Germany
 //
 // All rights reserved. Use of this source code is governed
 // by the new BSD license.
 
 // Package startup of the Tideland Go CouchDB Client provides a simple
-// mechanism for the initial creation an startup and maintenance of CouchDB
-// databases.
+// mechanism for a clean startup and maintenance of CouchDB databases.
 //
 // The major function is
 //
@@ -19,10 +18,10 @@
 //    func(cdb couchdb.CouchDB, v version.Version) (version.Version, error)
 //
 // When Run() is called it first checks if the database already exists and
-// if needed creates it. Then it adds an version document with the version
+// if needed creates it. Then it adds a version document with the version
 // 0.0.0. Now each step is called in order with the current version. It
 // can check if it has to modify the database (e.g. add design and other
-// document, add fields to existing documents, transform document, etc.),
+// document, add fields to existing documents, transform documents, etc.),
 // perform those changes, and return the new version. So the version
 // document will be updated and the next step performed.
 package startup
